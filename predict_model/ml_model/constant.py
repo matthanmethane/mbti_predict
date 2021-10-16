dd6b1ad3f2bb41579c528ff206488e10c7788831
Matt <donghan37@gmail.com>
2021-10-17 00:47:22 +0800

    change

---
+++ predict_model/ml_model/constant.py
@@ -0,0 +1,7 @@
+MBTI_DESCRIPTION =[
+    'Introversion (I) – Extroversion (E)' , 
+    'Intuition (N) – Sensing (S)', 
+    'Thinking (T) – Feeling (F)' , 
+    'Judging (J) – Perceiving (P)']
+
+MBTI_VEC_STR = 
